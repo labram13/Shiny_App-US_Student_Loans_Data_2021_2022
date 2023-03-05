afd45825eaf286b927222718d3ed1836c822fc6a
labram13 <labram@uw.edu>
2023-03-05 13:20:57 -0800

    added libraries and loaded csv

--- group_shiny_app/app.R
+++ group_shiny_app/app.R
@@ -1,4 +1,8 @@
 library(shiny)
+library(tidyverse)
+library(dplyr)
+
+student_loans <- read_delim("data/Student Loan Debt by School 2020-2021.csv")
 
 ui <- fluidPage(
     titlePanel("Student Loans 2020-2021"),
